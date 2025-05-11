b4841fcc053a58abef799b2b556cfe5538224e94
Jacob <jjacobdofficial@gmail.com>
2025-05-11 20:46:49 +0100

    Added MyUnionFind class for efficient cell clustering
    - Implements Union-Find (Disjoint Set Union) data structure
    - Supports path compression and union by size for fast grouping
    - Used to detect and merge connected blood cell regions

--- src/main/java/org/example/bloodcellanalyser/MyUnionFind.java
+++ src/main/java/org/example/bloodcellanalyser/MyUnionFind.java
@@ -1,4 +1,44 @@
 package org.example.bloodcellanalyser;
 
 public class MyUnionFind {
+    private int[] parent;
+    private int[] size;
+
+    public MyUnionFind(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (size[rootX] < size[rootY]) {
+                parent[rootX] = rootY;
+                size[rootY] += size[rootX];
+            } else {
+                parent[rootY] = rootX;
+                size[rootX] += size[rootY];
+            }
+        }
+    }
+
+    public int[] getParent() {
+        return parent;
+    }
+
+    public int[] getNodeSizes() {
+        return size;
+    }
 }
