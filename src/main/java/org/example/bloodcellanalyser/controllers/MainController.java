5106ffb37828d9004387779d6b29dac6db657517
Jacob <jjacobdofficial@gmail.com>
2025-05-11 22:39:24 +0100

    - The analyzecells method in MainController.java was given a less noiser adjustment for labelling without any overlaps

--- src/main/java/org/example/bloodcellanalyser/controllers/MainController.java
+++ src/main/java/org/example/bloodcellanalyser/controllers/MainController.java
@@ -6,7 +6,6 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
@@ -36,23 +35,10 @@ public class MainController implements Initializable {
     private MyUnionFind uf;
     private HashMap<Integer, BloodCellDetails> cells = new HashMap<>();
 
-    // Add to the MainController class (public section)
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public void setImageArray(int[] cellArray) {
-        this.cellArray = cellArray;
-    }
-
-    public void setPills(HashMap<Integer, BloodCellDetails> map) {
-        this.cells = map;
-    }
-
+    public void setWidth(int width) { this.width = width; }
+    public void setHeight(int height) { this.height = height; }
+    public void setImageArray(int[] cellArray) { this.cellArray = cellArray; }
+    public void setPills(HashMap<Integer, BloodCellDetails> map) { this.cells = map; }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -79,16 +65,19 @@ public class MainController implements Initializable {
     @FXML
     public void convertToTricolor() {
         WritableImage tricolor = new WritableImage(width, height);
+
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 Color c = pixelReader.getColor(x, y);
-                double hue = c.getHue(), sat = c.getSaturation(), bright = c.getBrightness();
-
+                double hue = c.getHue();
+                double sat = c.getSaturation();
+                double bright = c.getBrightness();
                 int index = y * width + x;
-                if (hue > 300 || (hue < 20 && bright > 0.6)) { // Red blood cells
+
+                if ((hue > 330 || hue < 20) && sat > 0.2 && bright > 0.4) {
                     tricolor.getPixelWriter().setColor(x, y, Color.RED);
                     cellArray[index] = -1;
-                } else if (hue > 200 && sat > 0.3) { // Purple nuclei (white blood cells)
+                } else if ((hue > 200 && hue < 280) && sat > 0.4 && bright < 0.8) {
                     tricolor.getPixelWriter().setColor(x, y, Color.PURPLE);
                     cellArray[index] = -2;
                 } else {
@@ -97,25 +86,26 @@ public class MainController implements Initializable {
                 }
             }
         }
+
         tricolorView.setImage(tricolor);
     }
 
     @FXML
     public void analyzeCells() {
-        // Union-Find red and purple clusters
+        // Union adjacent red/purple pixels
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 int idx = y * width + x;
                 if (cellArray[idx] < 0) {
-                    if (x + 1 < width && cellArray[y * width + (x + 1)] == cellArray[idx])
-                        uf.union(idx, y * width + (x + 1));
-                    if (y + 1 < height && cellArray[(y + 1) * width + x] == cellArray[idx])
-                        uf.union(idx, (y + 1) * width + x);
+                    if (x + 1 < width && cellArray[idx + 1] == cellArray[idx])
+                        uf.union(idx, idx + 1);
+                    if (y + 1 < height && cellArray[idx + width] == cellArray[idx])
+                        uf.union(idx, idx + width);
                 }
             }
         }
 
-        // Group pixels by root
+        // Group pixels into clusters
         Map<Integer, List<Integer>> clusters = new HashMap<>();
         for (int i = 0; i < cellArray.length; i++) {
             if (cellArray[i] < 0) {
@@ -130,10 +120,14 @@ public class MainController implements Initializable {
         gc.drawImage(originalImage, 0, 0);
 
         int redCount = 0, whiteCount = 0, label = 1;
+        cells.clear();
 
         for (Map.Entry<Integer, List<Integer>> entry : clusters.entrySet()) {
             List<Integer> indices = entry.getValue();
             int size = indices.size();
+
+            // 🔒 Skip tiny noise clusters
+            if (size < 20) continue;
 
             int minX = width, maxX = 0, minY = height, maxY = 0;
             for (int idx : indices) {
@@ -147,17 +141,18 @@ public class MainController implements Initializable {
 
             Color rectColor;
             String type;
+
             if (cellArray[indices.get(0)] == -2) {
                 rectColor = Color.PURPLE;
                 whiteCount++;
                 type = "white";
-            } else if (size < 60) { // Single red cell
+            } else if (size < 60) {
                 rectColor = Color.GREEN;
                 redCount++;
                 type = "red";
-            } else { // Cluster
+            } else {
                 rectColor = Color.BLUE;
-                redCount += Math.round(size / 50.0); // Estimate
+                redCount += Math.round(size / 50.0); // Cluster estimation
                 type = "red-cluster";
             }
 
@@ -171,9 +166,10 @@ public class MainController implements Initializable {
 
             if (showLabels.isSelected()) {
                 gc.setFont(Font.font(16));
-                gc.setFill(Color.BLACK);
+                gc.setFill(Color.BLACK); // Just the label in black
                 gc.fillText(String.valueOf(cell.getLabel()), minX, minY);
             }
+
         }
 
         canvas.snapshot(null, result);
