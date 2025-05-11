8b11b6ee15375be80485484f0c1eae290c8895a1
Jacob <jjacobdofficial@gmail.com>
2025-05-11 20:43:34 +0100

    Added ViewController for displaying image previews with navigation
    - Manages preview display of analyzed images in a  separate scene
    - Includes back button functionality to return to main view
    - Stores controller reference for external access

--- src/main/java/org/example/bloodcellanalyser/controllers/ViewController.java
+++ src/main/java/org/example/bloodcellanalyser/controllers/ViewController.java
@@ -1,4 +1,28 @@
 package org.example.bloodcellanalyser.controllers;
 
-public class ViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import org.example.bloodcellanalyser.HelloApplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewController implements Initializable {
+
+    public static ViewController viewController;
+
+    @FXML public ImageView imagePreview;
+    @FXML public Button backBtn;
+
+    @FXML
+    public void backToMain() {
+        HelloApplication.mainStage.setScene(HelloApplication.mainS);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        viewController = this;
+    }
 }
